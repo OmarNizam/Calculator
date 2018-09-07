d056b94aba824cf7c3635096bfc8a23eea235b83
OmarNizam <eng.omarnizam@gmail.com>
2018-09-07 11:03:10 +0200

    creat first look for the calculator with some style

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, Button, TouchableOpacity} from 'react-native';
 
 
 export default class App extends Component {
@@ -19,58 +19,103 @@ export default class App extends Component {
   }
 
   render() {
+    let rows = []
+    let nums =[[1, 2, 3], [4, 5, 6], [7, 8, 9], ['#',0,'=']]
+    for (let i = 0; i < 4; i++) {
+      let row = []
+      for (let j = 0; j < 3; j++ ) {
+        row.push(
+              <TouchableOpacity style={styles.btn}>
+                <Text style={styles.btntext}>{nums[i][j]}</Text>
+              </TouchableOpacity>)
+      }
+      rows.push(<View style={styles.row}>{row}</View>)
+    }
 
+    let operations = ["+", "-", "*", "/"]
+    let ops =[]
+    for (let i = 0; i < 4; i++) {
+      ops.push(
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.opstext}>{operations[i]}</Text>
+        </TouchableOpacity>
+      )
+    }
     return (
-
-        <View style={styles.container}>
-          <View style={styles.half1}>
-            <Text style={styles.text}> This is 1 </Text>
+      <View style={styles.container}>
+        <View style={styles.result}>
+          <Text style={styles.resultText}>11*11</Text>
+        </View>
+        <View style={styles.calculation}>
+        <Text style={styles.calculationText}>121</Text>
+        </View>
+        <View style={styles.buttons}>
+          <View style={styles.numbers}>
+          {rows}
           </View>
-          <View style={styles.half2}>
-            <View style={styles.half21}>
-              <Text style={styles.text}> This is 2/1 </Text>
-            </View>
-            <View style={styles.half22}>
-              <Text style={styles.text}> This is 2/2 </Text>
-            </View>
+          <View style={styles.operations}>
+            {ops}
           </View>
         </View>
-
-    )
+      </View>
+    );
   }
 }
 
-
-  const styles = StyleSheet.create({
-    container: {
-      flex:1,
-    },
-    half1: {
-      flex:1,
-      backgroundColor:'red',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    half2: {
-      flex:1,
-      flexDirection: 'row',
-      backgroundColor: 'blue',
-    },
-    half21: {
-      flex:1,
-      backgroundColor: 'green',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    half22: {
-      flex:1,
-      backgroundColor: 'black',
-      justifyContent: 'center',
-      alignItems: 'center'
-    },
-    text: {
-      color: 'white',
-      fontSize: 30,
-
-    }
-  })
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  btn: {
+    flex: 1,
+    alignItems: "center",
+    // alignSelf: "",
+    justifyContent: "center"
+  },
+  btntext: {
+    fontSize: 30
+  },
+  resultText: {
+    fontSize: 30,
+    color: 'white'
+  },
+  calculationText: {
+    fontSize: 24,
+    color: 'white'
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  result: {
+    flex: 2,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  buttons: {
+    flex: 7,
+    flexDirection: 'row'
+  },
+  numbers: {
+    flex: 3,
+    backgroundColor: 'yellow'
+  },
+  operations: {
+    flex: 1,
+    justifyContent: "space-around",
+    backgroundColor: 'black'
+  },
+  opstext: {
+    fontSize: 30,
+    color: 'white'
+  }
+})
